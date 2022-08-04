ff049494ffa367a455f7b92ae1ede9f3450f2cd1
HJH-08 <85354863+HJH-08@users.noreply.github.com>
2022-08-04 22:25:57 +0800

    Updated calendar planner
    Finished add task button
    Added functionality of task list button
    Added error checking if input for task is empty

--- calendar-planner.py
+++ calendar-planner.py
@@ -1,15 +1,79 @@
 # Importing The Essential Libraries
 from tkinter import *
 from tkcalendar import Calendar, DateEntry
+
+#ADD TASK PAGE
+
+def add_task():
+    def confirm_task():
+        no_task_label.pack_forget()
+        if user_task_input_entry.get() == '':
+            no_task_label.pack()
+            return
+        text = user_task_input_entry.get() + ' on ' + cal.get_date().strftime("%d/%m/%Y") + ' added to calendar'
+        confirmed_task_label.config(text=text)
+        confirmed_task_label.pack(pady=10)
+
+        main_cal.calevent_create(cal.get_date(), user_task_input_entry.get(), 'reminder')
+        main_cal.tag_config('reminder', background='red', foreground='yellow')
+        if cal.get_date() in task_dict:
+            task_dict[cal.get_date()].append(user_task_input_entry.get())
+        else:
+            task_dict[cal.get_date()] = [user_task_input_entry.get()]
+
+        back_to_calendar_button.pack(padx=10, side='left')
+        enter_another_task_button.pack(padx=10, side='right')
+
+    def create_another_task():
+        user_task_input_entry.delete(0, END)
+        confirmed_task_label.config(text='')
+        back_to_calendar_button.pack_forget()
+        enter_another_task_button.pack_forget()
+
+    top = Toplevel(tk)
+    top.geometry('400x400')
+
+    date_from_main = main_cal.get_date()
+    user_input_label = Label(top, text='Enter the task and its date: ')
+    user_input_label.pack(padx=10, pady=20)
+
+    user_task_input_entry = Entry(top, width=30)
+    user_task_input_entry.pack(pady=20)
+
+    cal = DateEntry(top, width=30, background='darkblue',
+                    foreground='white', borderwidth=2, date_pattern='dd/mm/yyyy')
+    cal.set_date(date_from_main)
+    cal.pack(padx=10, pady=10)  
+
+    confirm_button = Button(top, text="Confirm", command=confirm_task)
+    confirm_button.pack(pady=10)
+    no_task_label = Label(top, text="Input some task please!")
+
+    confirmed_task_label = Label(top, wraplength=300)
+    back_to_calendar_button = Button(top, text="Back to Calendar", command=top.destroy)
+    enter_another_task_button = Button(top, text="Create Another Task", command=create_another_task)
  
+def print_tasks():
+    task_text = ''
+    for day in task_dict:
+        date_tasks = ''
+        for task in task_dict[day]:
+            date_tasks += f', {task}'
+        task_text += f'On {day}: {date_tasks[2:]}\n'
+    Label(tk, text=task_text).grid(row=3)
+
+def delete_tasks():
+    pass;
+
 # Create The Gui Object
 tk = Tk()
+tk.update_idletasks()
  
 # Set the geometry of the GUI Interface
 tk.geometry("700x700")
  
 # Add the Calendar module
-cal = Calendar(
+main_cal = Calendar(
 tk, selectmode = 'day', date_pattern = 'dd-mm-yyyy', 
 background = '#00008B', foreground = 'white',
 headersbackground = '#A7C7E7', headersforeground = 'black',
@@ -17,7 +81,7 @@ weekendbackground = '#f6f6f6', othermonthbackground = '#ececec',
 othermonthwebackground = '#e2e2e2', cursor='hand1' )
  
 #cal.pack(pady = 100, fill="both", expand=True)
-cal.grid(row=0, column=0, columnspan=2, sticky='WENS', pady = 50)
+main_cal.grid(row=0, column=0, columnspan=2, sticky='WENS', pady = 50)
 tk.columnconfigure(0, weight=1)
 tk.columnconfigure(1, weight=1)
 tk.rowconfigure(0, weight=1)
@@ -25,47 +89,17 @@ tk.rowconfigure(1, weight=1)
  
 # Function to grab the selected date
 def grad_date():
-    date.config(text = "Selected Date is: " + cal.get_date())
+    date.config(text = "Selected Date is: " + main_cal.get_date())
  
 # Adding the Button and Label
 Button(tk, text = "Get Date", command = grad_date).grid(row=1, column=0)
 
-def add_task():
-    def create_another_task():
-        top.destroy()
-        add_task()
-
-    def confirm_task():
-        text = user_task_input.get().capitalize() + ' on ' + cal.get_date().strftime("%m/%d/%Y") + ' added to calendar'
-        print(text)
-        confirmed_task_label = Label(top, text=text, wraplength=300)
-        confirmed_task_label.pack(pady=10)
-
-        back_to_calendar_button = Button(top, text="Back to Calendar", command=top.destroy)
-        back_to_calendar_button.pack(padx=10, side='left')
-
-        enter_another_task_button = Button(top, text="Create Another Task", command=create_another_task)
-        enter_another_task_button.pack(padx=10, side='right')
-
-    top = Toplevel(tk)
-    top.geometry('400x400')
-
-    user_input_label = Label(top, text='Enter the task and its date: ')
-    user_input_label.pack(padx=10, pady=20)
-
-    user_task_input = Entry(top, width=30)
-    user_task_input.pack(pady=20)
-
-    cal = DateEntry(top, width=30, background='darkblue',
-                    foreground='white', borderwidth=2)
-    cal.pack(padx=10, pady=10)
-
-    confirm_button = Button(top, text="Confirm", command=confirm_task)
-    confirm_button.pack(pady=10)
-
-
-
 Button(tk, text="Add Task", command=add_task).grid(row=1, column=1)
+task_dict = {}
+
+Button(tk, text="Task List", command=print_tasks).grid(row=2, column=0)
+
+Button(tk, text="Delete Tasks", command=delete_tasks).grid(row=2, column=1)
  
 date = Label(tk, text = "")
 date.grid(row = 100, column = 0)
