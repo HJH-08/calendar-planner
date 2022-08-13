2815ec6cffaf51f829dd364177b109c2432ec4b1
HJH-08 <85354863+HJH-08@users.noreply.github.com>
2022-08-13 21:55:57 +0800

    Update calendar planner
    Added an instructions page
    Tidied formatting on the button windows and root window

--- calendar-planner.py
+++ calendar-planner.py
@@ -1,20 +1,114 @@
 # Importing The Essential Libraries
-from ast import If
 from tkinter import *
 from tkcalendar import Calendar, DateEntry
 from datetime import datetime
 from tkinter import font
 
-#ADD TASK PAGE
 def add_back_to_calendar_button(master):
     back_to_calendar_button = Button(master, text="Back to Calendar", command=master.destroy)
     back_to_calendar_button.pack()
+#ADD TASK PAGE
+#SAVE INFO INTO EXCEL?
+
+def show_instructions():
+    top = Toplevel(tk)
+    top.geometry('600x700')
+
+
+    def hide_intro():
+        intro_label.config(text='')
+        intro_button.config(text='Introduction', command=show_intro)
+    def show_intro():
+        intro_label.config(text=intro_text)
+        intro_button.config(text='Hide Introduction', command=hide_intro)
+    intro_text = ("This is a calendar app planner. Blue on the calendar means selected date, and the default selected date is" 
+    " today. Red on the calendar means there is a reminder on that date. Hover over that date and wait a few seconds for the" 
+    " task(s) to show up.")
+    intro_label = Label(top, wraplength=600, text='', font=("Helvetica 12"))
+    intro_button = Button(top, text="Introduction", command=show_intro)
+    intro_button.pack(pady=(30,0))
+    intro_label.pack(pady=5)
+
+    def hide_addtask():
+        addtask_label.config(text='')
+        addtask_button.config(text='About Add Tasks', command=show_addtask)
+    def show_addtask():
+        addtask_label.config(text=addtask_text)
+        addtask_button.config(text='Hide Add Tasks', command=hide_addtask)
+    addtask_text = ("Add any task on any date using the add task button. The calendar will be updated when the added task is" 
+    " confirmed. You cannot add any repeated tasks in any single day. Tasks are case sensitive.")
+    addtask_label = Label(top, wraplength=600, text='', font=("Helvetica 12"))
+    addtask_button = Button(top, text="About Add Tasks", command=show_addtask)
+    addtask_button.pack()
+    addtask_label.pack(pady=5)
+
+    def hide_deletetask():
+        deletetask_label.config(text='')
+        deletetask_button.config(text='About Delete Tasks', command=show_deletetask)
+    def show_deletetask():
+        deletetask_label.config(text=deletetask_text)
+        deletetask_button.config(text='Hide Delete Tasks', command=hide_deletetask)
+    deletetask_text = ("Select any task from the task list to delete. Upon confirmation, the task will be removed permenantly."
+    " The calendar will automatically be updated.")
+    deletetask_label = Label(top, wraplength=600, text='', font=("Helvetica 12"))
+    deletetask_button = Button(top, text="About Delete Tasks", command=show_deletetask)
+    deletetask_button.pack()
+    deletetask_label.pack(pady=5)
+
+    def hide_tasklist():
+        tasklist_label.config(text='')
+        tasklist_button.config(text='About Tasks Lists', command=show_tasklist)
+    def show_tasklist():
+        tasklist_label.config(text=tasklist_text)
+        tasklist_button.config(text='Hide Tasks Lists', command=hide_tasklist)
+    tasklist_text = "This button shows you all your tasks that you have inputted in the planner."
+    tasklist_label = Label(top, wraplength=600, text='', font=("Helvetica 12"))
+    tasklist_button = Button(top, text="About Tasks Lists", command=show_tasklist)
+    tasklist_button.pack()
+    tasklist_label.pack(pady=5)
+
+    def hide_taskforday():
+        taskforday_label.config(text='')
+        taskforday_button.config(text='About Tasks For The Day', command=show_taskforday)
+    def show_taskforday():
+        taskforday_label.config(text=taskforday_text)
+        taskforday_button.config(text='Hide Tasks For The Day', command=hide_taskforday)
+    taskforday_text = ("This button will show you what tasks you have on any selected day. If you do not have any tasks," 
+    " it will say that your task list is empty. You can choose to complete a task. By completing a task, the task will appear" 
+    " cancelled, but it will still appear on the calendar. In order to delete a specific task, go to the delete tasks button"
+    " to delete the task. All completed tasks are grouped together at the buttom according to their date. You can choose to" 
+    " delete all completed tasks at one go too.")
+    taskforday_label = Label(top, wraplength=600, text='', font=("Helvetica 12"))
+    taskforday_button = Button(top, text="About Tasks For The Day", command=show_taskforday)
+    taskforday_button.pack()
+    taskforday_label.pack(pady=5)
+
+    def hide_taskforweek():
+        taskforweek_label.config(text='')
+        taskforweek_button.config(text='About Tasks For The Week', command=show_taskforweek)
+    def show_taskforweek():
+        taskforweek_label.config(text=taskforweek_text)
+        taskforweek_button.config(text='Hide Tasks For The Week', command=hide_taskforweek)
+    taskforweek_text = ("This button shows you the tasks you have from the selected day to the end of that particular week."
+    " Functionalities are the same as the Show Task for Day button.")
+    taskforweek_label = Label(top, wraplength=600, text='', font=("Helvetica 12"))
+    taskforweek_button = Button(top, text="About Tasks For The Week", command=show_taskforweek)
+    taskforweek_button.pack()
+    taskforweek_label.pack(pady=5)
+
+    add_back_to_calendar_button(top)
+
+
 
 def strike(text):
     result = ''
     for c in text:
         result = result + c + '\u0336'
     return result
+
+def unstrike(text):
+    newtext = text.replace('\u0336', '')
+    return newtext
 
 def add_task():
     def confirm_task():
@@ -84,10 +178,11 @@ def add_task():
 
 def show_task_list():
     top = Toplevel(tk)
-    top.geometry('400x400')
+    top.geometry('500x500')
 
     def items_selected(event):
-        task_info_label.config(text=task_listbox.get(ANCHOR))
+        if task_listbox.get(ANCHOR):
+            task_info_label.config(text=unstrike(task_listbox.get(ANCHOR)))
         task_info_label.pack()
 
 
@@ -101,7 +196,7 @@ def show_task_list():
     tasks_list_scrollbar.config(command=task_listbox.yview)
     task_listbox.bind('<<ListboxSelect>>', items_selected)
 
-    task_info_label = Label(top, text='')
+    task_info_label = Label(top, text='', font='Helvetica 12')
 
     task_dict_sorted = {}
     for date in sorted(task_dict):
@@ -113,12 +208,15 @@ def show_task_list():
     for day in task_dict_sorted:
         for task in task_dict_sorted[day]:
             if '\u0336' in task:
-                completed_task = day.strftime("%d-%m-%Y") + ':' + task
+                completed_task = day.strftime("%d-%m-%Y") + ': ' + task
                 task_listbox.insert(END, strike(completed_task))
     
     tasks_list_scrollbar.pack(side=RIGHT, fill=Y)
     tasks_list_frame.pack()
     task_listbox.pack(pady=15)
+    task_info_label.pack(pady=15)
+    if task_listbox.size() == 0:
+        task_info_label.config(text="Your task list is empty. Input some tasks in the add tasks tab!")
     add_back_to_calendar_button(top)
     
 
@@ -127,10 +225,15 @@ def delete_tasks_window():
     
     def delete_task():
         if task_listbox.get(ANCHOR):
-            task_date, task = task_listbox.get(ANCHOR).split(': ', 1)
+            if '\u0336' in task_listbox.get(ANCHOR):
+                task_date, task_unstrike = unstrike(task_listbox.get(ANCHOR)).split(': ', 1);
+                task = strike(task_unstrike)
+
+            if '\u0336' not in task_listbox.get(ANCHOR):
+                task_date, task = task_listbox.get(ANCHOR).split(': ', 1)
+                
             task_date_object = datetime.strptime(task_date, '%d-%m-%Y').date()
-            global task_dict
-            global task_dict_sorted
+
             task_dict[task_date_object].remove(task)
             #TASK DICT UPDATES = TASK DICT SORTED ALSO UPDATES?
             #task_dict_sorted[task_date_object].remove(task) 
@@ -152,11 +255,11 @@ def delete_tasks_window():
         task_delete_label.config(text=f'{task_listbox.get(ANCHOR)} is going to be deleted')
 
     top = Toplevel(tk)
-    top.geometry('400x400')
+    top.geometry('500x500')
     tasks_list_frame = Frame(top)
     tasks_list_scrollbar = Scrollbar(tasks_list_frame, orient=VERTICAL)
-    task_list_label = Label(top, text="Delete from your task list", anchor=CENTER, font=("Helvetica 18 underline"))
-    task_list_label.pack(pady = 20)
+    task_list_label = Label(top, text="Delete from your task list", anchor=CENTER, font=("Helvetica 15 underline"))
+    task_list_label.pack(pady = 10)
     global task_listbox
     task_listbox = Listbox(tasks_list_frame, font=big_font, width=40, yscrollcommand=tasks_list_scrollbar.set)
     task_listbox.bind('<<ListboxSelect>>', items_selected)
@@ -169,7 +272,7 @@ def delete_tasks_window():
     for day in task_dict_sorted:
         for task in task_dict_sorted[day]:
             if '\u0336' in task:
-                completed_task = day.strftime("%d-%m-%Y") + ':' + task
+                completed_task = day.strftime("%d-%m-%Y") + ': ' + task
                 task_listbox.insert(END, strike(completed_task))
         
     tasks_list_scrollbar.pack(side=RIGHT, fill=Y)
@@ -181,7 +284,7 @@ def delete_tasks_window():
     if task_listbox.size() == 0:
         task_delete_label.config(text="Your task list is empty!")
         delete_button['state'] = 'disable'
-    delete_button.pack()
+    delete_button.pack(pady=10)
     add_back_to_calendar_button(top)
 
 
@@ -191,6 +294,7 @@ def delete_tasks_window():
 
 def show_tasks_for_day():
     top = Toplevel(tk)
+    top.geometry('550x550')
     tasks_list_frame = Frame(top)
     tasks_list_scrollbar = Scrollbar(tasks_list_frame, orient=VERTICAL)
     task_complete_label = Label(top, text='')
@@ -263,11 +367,10 @@ def show_tasks_for_day():
                 complete_button['state'] = 'normal'
                 task_complete_label.config(text=f'Is {task_listbox_day.get(ANCHOR)} completed?')
     
-    top.geometry('400x400')
    
     
     task_list_label = Label(top, text=f"Tasks for day: {date}", anchor=CENTER, font=("Helvetica 18 underline"))
-    task_list_label.pack(pady = 20)
+    task_list_label.pack(pady = 15)
     
     task_listbox_day.bind('<<ListboxSelect>>', items_selected)
     tasks_list_scrollbar.config(command=task_listbox_day.yview)
@@ -285,11 +388,9 @@ def show_tasks_for_day():
                 delete_completed_tasks_button['state'] = 'normal'
             if '\u0336' not in task:
                 complete_button['state'] = 'normal'
-    complete_button.pack()
-    delete_completed_tasks_button.pack()
+    complete_button.pack(pady=12)
+    delete_completed_tasks_button.pack(pady=12)
     add_back_to_calendar_button(top)
-
-
 
 
 
@@ -308,6 +409,7 @@ def show_tasks_for_week():
     
 
     top = Toplevel(tk)
+    top.geometry('550x550')
     tasks_list_frame = Frame(top)
     tasks_list_scrollbar = Scrollbar(tasks_list_frame, orient=VERTICAL)
     task_list_label = Label(top, text=f"Task List for the week: {selected_date} to {end_date}", anchor=CENTER, font=("Helvetica 14 underline"))
@@ -423,8 +525,8 @@ def show_tasks_for_week():
                 delete_completed_tasks_button['state'] = 'normal'
             if '\u0336' not in task:
                 complete_button['state'] = 'normal'
-    complete_button.pack()
-    delete_completed_tasks_button.pack()
+    complete_button.pack(pady=12)
+    delete_completed_tasks_button.pack(pady=12)
     add_back_to_calendar_button(top)
 
 
@@ -432,11 +534,27 @@ def show_tasks_for_week():
 # Create The Gui Object
 tk = Tk()
 tk.update_idletasks()
+column_number = 3
+row_number = 5
+for _ in range(0, column_number):
+    Grid.columnconfigure(tk, _, weight=1)
+for _ in range(0, row_number):
+    Grid.rowconfigure(tk, _, weight=1)
+    if _ == 2:
+        Grid.rowconfigure(tk, _, weight=8)
+
 big_font = font.Font(size=15)
  
 # Set the geometry of the GUI Interface
-tk.geometry("700x700")
+tk.geometry("500x500")
  
+title_label = Label(tk, text='Calendar Planner', font=("Helvetica 20 bold underline"), anchor='center')
+title_label.grid(row=0, column=1)#, columnspan=3)
+
+details_button = Button(tk, text='Show information', command=show_instructions, anchor='center')
+details_button.grid(row=1, column=1)
+
+
 # Add the Calendar module
 main_cal = Calendar(
 tk, selectmode = 'day', date_pattern = 'dd-mm-yyyy', 
@@ -445,33 +563,21 @@ headersbackground = '#A7C7E7', headersforeground = 'black',
 weekendbackground = '#f6f6f6', othermonthbackground = '#ececec',
 othermonthwebackground = '#e2e2e2', cursor='hand1' )
  
-#cal.pack(pady = 100, fill="both", expand=True)
-main_cal.grid(row=0, column=0, columnspan=2, sticky='WENS', pady = 50)
-tk.columnconfigure(0, weight=1)
-tk.columnconfigure(1, weight=1)
-tk.rowconfigure(0, weight=1)
-tk.rowconfigure(1, weight=1)
- 
-# Function to grab the selected date
-def grad_date():
-    date.config(text = "Selected Date is: " + main_cal.get_date())
- 
-# Adding the Button and Label
-Button(tk, text = "Get Date", command = grad_date).grid(row=1, column=0)
+main_cal.grid(row=2, column=0, columnspan=3, sticky='WENS', pady = 50)
 
-Button(tk, text="Add Task", command=add_task).grid(row=1, column=1)
+
+
+Button(tk, text="Add Task", command=add_task).grid(row=3, column=0)
 task_dict = {}
 task_dict_sorted = {}
 
-Button(tk, text="Task List", command=show_task_list).grid(row=2, column=0)
+Button(tk, text="Task List", command=show_task_list).grid(row=3, column=2)
 
-Button(tk, text="Delete Tasks", command=delete_tasks_window).grid(row=2, column=1)
+Button(tk, text="Delete Tasks", command=delete_tasks_window).grid(row=3, column=1)
 
-Button(tk, text="Tasks for the selected day", command=show_tasks_for_day).grid(row=1, column=3)
-Button(tk, text="Tasks for the selected week", command=show_tasks_for_week).grid(row=2, column=3)
- 
-date = Label(tk, text = "")
-date.grid(row = 100, column = 0)
+Button(tk, text="Tasks for the day", command=show_tasks_for_day).grid(row=4, column=0, pady=20)
+Button(tk, text="Tasks for the week", command=show_tasks_for_week).grid(row=4, column=1, pady=20)
+
  
 # Execute Tkinter
 tk.mainloop()
